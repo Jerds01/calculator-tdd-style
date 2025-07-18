a16592cba1aaca6c24f84727204d04611afc68de
JeromeChristopher <JeromeChristopher@gcash.com>
2025-07-18 11:46:12 +0800

    Test Cases for Subtraction Method

--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -17,12 +17,12 @@ public class CalculatorTest {
     @Test
     public void testSumFloat(){
         float sum = calc.sum(1.1f, 5.1f);
-        assertEquals(6.2f, sum, .001);
+        assertEquals(6.2f, sum, .0001);
     }
     @Test
     public void testSumDouble(){
         double sum = calc.sum(1.1, 5.1);
-        assertEquals(6.2, sum, .001);
+        assertEquals(6.2, sum, .0001);
     }
     @Test
     public void testSumLong(){
@@ -32,9 +32,33 @@ public class CalculatorTest {
     @Test
     public void testSumShort(){
         short sum = calc.sum( (short)1, (short )5);
-        assertEquals(6, sum, 0.001);
+        assertEquals(6, sum, 0.0001);
     }
 
-
+    @Test
+    public void testSubtractionInt(){
+        int difference= calc.subtract(6,2);
+        assertEquals(4, difference);
+    }
+    @Test
+    public void testSubtractionFloat(){
+        int difference= calc.subtract(6.5f,2.3f);
+        assertEquals(4.2f, difference,  0.0001);
+    }
+    @Test
+    public void testSubtractionDouble(){
+        double difference= calc.subtract(6.5,2.3);
+        assertEquals(4.2, difference, 0.0001);
+    }
+    @Test
+    public void testSubtractionLong(){
+        long difference= calc.subtract((long) 6, (long) 2);
+        assertEquals(4, difference,  0.0001);
+    }
+    @Test
+    public void testSubtractionShort(){
+        short difference= calc.subtract((short) 6.2, (short) 2.1);
+        assertEquals(4.1, difference,  0.0001);
+    }
 
 }
